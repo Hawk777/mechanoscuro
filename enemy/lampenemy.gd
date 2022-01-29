5ad9365465dd19b36296e4425630b6fcd1dd6687
Agagne00 <60492267+Agagne00@users.noreply.github.com>
2022-01-29 15:41:39 -0800

    lampenemy working
    lampenemy turns when there's a wall in front of it

--- enemy/lampenemy.gd
+++ enemy/lampenemy.gd
@@ -1,9 +1,20 @@
 extends "res://enemy/enemy.gd"
 class_name lampEnemy
 
-var directionFacing:=Vector2(0,0) # keeps moving in that direction until it hits a wall, then turns.
+export(NodePath) var tile_grid_path: NodePath
+
+onready var tile_grid := get_node(tile_grid_path) as TileGrid
+
+var directionFacing:=Vector2(1,0) # keeps moving in that direction until it hits a wall, then turns.  Default moves right.
 
 func _on_Player_moved():
 	if directionFacing!=Vector2(0,0):
 		# check that the tile immediately in front of this has no collision box.  If so, advance.  If not, multiply directionFacing by -1 and advance.
-		
+		var new_pos = self.position + directionFacing*Constants.TILE_SIZE
+		if (tile_grid.coord_is_passable(new_pos[0] / 64, new_pos[1] / 64)):
+			self.position = new_pos
+		else:
+			flip_h=!flip_h
+			directionFacing*=-1
+			new_pos = self.position + directionFacing*Constants.TILE_SIZE
+			self.position=new_pos
