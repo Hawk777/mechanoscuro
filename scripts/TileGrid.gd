d666d73b674140e40203d9a608606402d73e95cf
Agagne00 <60492267+Agagne00@users.noreply.github.com>
2022-01-28 21:59:25 -0800

    Merge branch 'main' of https://github.com/Hawk777/ggj2022

---
+++ scripts/TileGrid.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+const GRID_HEIGHT=9
+const GRID_WIDTH=16
+var Grid = []
+# these are sample numbers based on my 64x64 tilemap.  If the size of the tiles changes or
+# you need more, change these.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for n in range(GRID_WIDTH):
+		var Subgrid = []
+		for m in range(GRID_HEIGHT):
+			print(TileMap.get_cell(n,m))
+			#Subgrid.append()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
