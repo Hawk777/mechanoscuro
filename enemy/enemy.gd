3ef3f92a1b32a7b6d7b30a9d41c750f51bb43a6d
Agagne00 <60492267+Agagne00@users.noreply.github.com>
2022-01-29 21:27:18 -0800

    sounds implemented
    fixed an issue where they looped when they shouldn't

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -4,7 +4,12 @@ class_name enemy
 
 export(NodePath) var player_path
 export(NodePath) var tile_grid_path
-
+export(AudioStream) var alert_sound
+export(AudioStream) var dealert_sound
+export(AudioStream) var step_sound
+onready var alert_player = get_node("MonsterAlertPlayer") as AudioStreamPlayer2D
+onready var dealert_player = get_node("MonsterDeAlertPlayer") as AudioStreamPlayer2D
+onready var step_player = get_node("MonsterStepPlayer") as AudioStreamPlayer2D
 onready var player := get_node(player_path) as Node2D
 onready var tilemap := get_node(tile_grid_path) as TileGrid
 
@@ -15,6 +20,9 @@ var preferredAxis="y" # preferred movement axis, for making enemies move how the
 
 
 func _ready() -> void:
+	alert_player.stream=alert_sound
+	dealert_player.stream=dealert_sound
+	step_player.stream=step_sound
 	tilemap.get_tilev(tilemap.world_to_map(tilemap.to_local(global_position))).occupant = self
 
 
@@ -58,22 +66,30 @@ func _on_Player_moved():
 				if motion==Vector2.ZERO:
 					motion=checkMoveX()
 			_move_by(motion)
+			if motion!=Vector2.ZERO:
+				step_player.play()
 		# check to see if Player is in light and in line of sight.  If so, turn on alert and update animation and update lastKnownCoords
 		# possibly just cheat and check exactly 3 or 4 tiles in each direction?  That should probably work, even if it's not all the way accurate
 		var player_grid := tilemap.world_to_map(tilemap.to_local(player.global_position))
 		var player_tile := tilemap.get_tilev(player_grid)
 		var self_grid := tilemap.world_to_map(tilemap.to_local(global_position))
 		if abs(player_grid.x-self_grid.x) < 4 and player_grid.y==self_grid.y and player_tile.isLit:
+			if !alert:
+				alert_player.play()
 			alert=true
 			play("alert")
 			lastKnownCoords=player_grid
 			#lastKnownCoords currently doesn't do anything.
 		if abs(player_grid.y-self_grid.y) < 4 and player_grid.x==self_grid.x and player_tile.isLit:
+			if !alert:
+				alert_player.play()
 			alert=true
 			play("alert")
 			lastKnownCoords=player_grid
 		# check to see if Player is in darkness.  If so, turn off alert and update animation.
 		if !player_tile.isLit:
+			if alert:
+				dealert_player.play()
 			alert=false
 			play("idle")
 
