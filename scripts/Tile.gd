ebee5f2dc803f332b87e2abf2b8d7b5f578319b8
Christopher Head <chead@chead.ca>
2022-01-29 17:23:41 -0800

    Reformat and remove template comments

--- scripts/Tile.gd
+++ scripts/Tile.gd
@@ -1,18 +1,8 @@
-extends Node
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 class_name Tile
+extends Node
 
 var isLit := false
 var light_toggles := false
 var xPos: int
 var yPos: int
 var passable := true
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
